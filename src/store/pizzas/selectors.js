c0de41c25e05f50e8239f3c179876e2521a88e74
matiasgarcia91 <matiasigarcia91@gmail.com>
2022-05-03 10:31:33 +0200

    Modify selector to sort pizzas

--- src/store/pizzas/selectors.js
+++ src/store/pizzas/selectors.js
@@ -1,1 +1,4 @@
-export const selectPizzas = (reduxState) => reduxState.pizzas.allPizzas;
+export const selectPizzas = (reduxState) => {
+  const clonedArray = [...reduxState.pizzas.allPizzas];
+  return clonedArray.sort((a, b) => b.bought - a.bought);
+};
