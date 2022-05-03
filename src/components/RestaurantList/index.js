2e87b3a2fab96e1ae2e79e4b8bcabc4aea1530d3
matiasgarcia91 <matiasigarcia91@gmail.com>
2022-05-03 11:31:56 +0200

    Add selectRestaurantsWithPizzas

--- src/components/RestaurantList/index.js
+++ src/components/RestaurantList/index.js
@@ -1,18 +1,27 @@
 // src/components/PizzaList.js
 import { useSelector } from "react-redux";
-import { selectRestaurants } from "../../store/restaurants/selectors";
+import { selectRestaurantsWithPizzas } from "../../store/selectors";
 
 export function RestaurantList() {
-  const restaurants = useSelector(selectRestaurants);
+  const restaurants = useSelector(selectRestaurantsWithPizzas);
 
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        {restaurants.map((restaurant) => (
-          <li key={restaurant.id}>{restaurant.name}</li>
-        ))}
-      </ul>
+      <div style={{ display: "flex", justifyContent: "flex-start" }}>
+        <ul>
+          {restaurants.map((restaurant) => (
+            <li key={restaurant.id}>
+              <strong>{restaurant.name}</strong>
+              <ul>
+                {restaurant.pizzas.map((p) => (
+                  <li>{p.name}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
