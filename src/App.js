d8a419d4be4f3d0ecd126c2d1edc515433f1d687
matiasgarcia91 <matiasigarcia91@gmail.com>
2022-05-03 10:44:13 +0200

    Make AddPizzaForm work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { PizzaList } from "./components";
+import { PizzaList, AddPizzaForm } from "./components";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <PizzaList />
+      <AddPizzaForm />
     </div>
   );
 }
